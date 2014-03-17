557ba3abf28091ec002aa886e739b6ebbdb5f605
dni <dni.khr@gmail.com>
2014-03-17 13:33:12 +0100

    gitignore, fix pdf creation

--- components/backend/modules/magazine/generators/PrintGenerator.js
+++ components/backend/modules/magazine/generators/PrintGenerator.js
@@ -3,8 +3,9 @@ var scissors = require('scissors');
 var path = require('path');
 var mime = require('mime');
 var phantom = require("phantom");
+var port = 40000;
 
-module.exports.download = function(req, res){	
+module.exports.download = function(req, res){
 	var pages = [];
 	fs.unlink("./public/books/" + req.params.title + "/pdf/Print.pdf", function(){
 		fs.readdir("./public/books/" + req.params.title + "/pdf/", function(err, files) {
@@ -23,6 +24,32 @@ module.exports.download = function(req, res){
 		});
 	});
 };
+
+module.exports.generatePage = function(file, magazine) {
+	phantom.create({port: port++}, function(ph){
+		ph.createPage(function(page) {
+			return page.open("./public/books/" + magazine.title + "/hpub/" + file, function(status) {
+				if (status !== "success") {
+					console.log("unable to open page");
+					ph.exit();
+				}
+				return page.evaluate(function() {
+					return document;
+				}, function(result) {
+					var split = file.split(".");
+					page.set('paperSize', {
+						format : "A5",
+						orientation : 'portrait',
+						margin : '0'
+					});
+					// page.paperSize = { format: "A5", orientation: 'portrait', margin: '1cm' };
+					page.render("./public/books/" + magazine.title + "/pdf/" + split.shift() + ".pdf");
+				});
+			});
+		});
+	});
+};
+
 
 module.exports.generate = function(magazine) {
 	fs.readdir("./public/books/" + magazine.title + "/hpub/", function(err, files) {
@@ -51,7 +78,7 @@ module.exports.generate = function(magazine) {
 					});
 				});
 			});
-			
+
 		};
 		for (key in files) {
 			if (files[key].match(/.html/g)) {
