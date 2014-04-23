d7918649ce7b3f2fda7bb937d0b2922bf59b64d6
drsdrs <ae@dnilabs.com>
2014-04-23 18:04:41 +0200

    install.js

--- install.js
+++ install.js
@@ -1,5 +1,43 @@
-// * npm install -g bower jake
-// * npm install
-// * bower install
-// * cd bower_components/tinymce && jake && cd ../..
-// * git clone https://github.com/bakerframework/baker.git baker-master
+var child_process = require('child_process').spawn;
+
+function bakermasterClone() {
+	var rmBakerDir = child_process('rm', ['-r', 'baker-master']);
+	rmBakerDir.on('exit', function (code) {
+		var bakermasterClone = child_process('git', ['clone', 'https://github.com/bakerframework/baker.git', 'baker-master']);
+		bakermasterClone.stdout.on('data', function (data) { console.log(data.toString()); });
+		bakermasterClone.stderr.on('data', function (data) { console.log(data.toString()); });
+		bakermasterClone.on('close', function (code) {
+			var rmDemoBook = child_process('rm', ['-r', './baker-master/books/a-study-in-scarlet']);
+			console.log('bakermasterClone exited with code ' + code);
+			console.log('Installation is complete');
+		});
+	});
+}
+
+function jakeInstall() {
+	var jakeInstall = child_process('jake', [], {cwd:'./bower_components/tinymce'});
+	jakeInstall.stdout.on('data', function (data) { console.log(data.toString()); });
+	jakeInstall.stderr.on('data', function (data) { console.log(data.toString()); });
+	jakeInstall.on('close', function (code) {
+		console.log('jakeInstall exited with code ' + code);
+		bakermasterClone();
+	});
+}
+
+function bowerInstall() {
+	var bowerInstall = child_process('bower', ['install']);
+	bowerInstall.stdout.on('data', function (data) { console.log(data.toString()); });
+	bowerInstall.stderr.on('data', function (data) { console.log(data.toString()); });
+	bowerInstall.on('close', function (code) {
+		console.log('bowerInstall exited with code ' + code);
+		jakeInstall();
+	});
+}
+
+var npmInstall = child_process('npm', ['install']);
+npmInstall.stdout.on('data', function (data) { console.log(data.toString()); });
+npmInstall.stderr.on('data', function (data) { console.log(data.toString()); });
+npmInstall.on('close', function (code) {
+	console.log('npmInstall exited with code ' + code);
+	bowerInstall();
+});
