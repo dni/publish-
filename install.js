a26074e59ec46c0b84e05cb45cd841919693fc94
dni <dni.khr@gmail.com>
2014-04-23 13:21:10 +0200

    install

---
+++ install.js
@@ -0,0 +1,5 @@
+// * npm install -g bower jake
+// * npm install
+// * bower install
+// * cd bower_components/tinymce && jake && cd ../..
+// * git clone https://github.com/bakerframework/baker.git baker-master
