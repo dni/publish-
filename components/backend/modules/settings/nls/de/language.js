a1bc4b0a3117f660b9b836303449f65f643f3b68
drsdrs <ae@dnilabs.com>
2014-06-17 09:54:09 +0200

    file description for settings

--- components/backend/modules/settings/nls/de/language.js
+++ components/backend/modules/settings/nls/de/language.js
@@ -2,4 +2,7 @@ define({
     "headline": "Einstellungen",
     "deleteSetting": "hat Einstellung gelöscht",
     "updateSetting": "hat Einstellung aktualisiert"
+    "fileDescription":
+        "Template keys fuer icon generator sind 'logo','icon','background'<br>'logo' im verhaeltnis 1024x640<br>'icon' verhaeltnis 286x286<br>'background' size = 2048x2048",
+
 });
--- components/backend/modules/settings/nls/language.js
+++ components/backend/modules/settings/nls/language.js
@@ -2,7 +2,10 @@ define({
     "root": {
     	"headline": "Settings",
     	"deleteSetting": "deleted setting",
-        "updateSetting": "updated setting"
+      "fileDescription":
+        "Use template keys for icon-generator 'logo','icon','background'<br>'logo' size must match 1024x640<br>'icon' size = 286x286<br>'background' size = 2048x2048",
+
+      "updateSetting": "updated setting"
     },
     "de": true
 });
